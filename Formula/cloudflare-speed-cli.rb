c83a69176adc3928bc9914dbec207b20ef1c8d0f
George L. Yermulnik <yz@yz.kiev.ua>
2026-03-18 22:29:39 +0200

    [`cloudflare-speed-cli`] Bump to latest version and support install on Linux

--- Formula/cloudflare-speed-cli.rb
+++ Formula/cloudflare-speed-cli.rb
@@ -1,16 +1,27 @@
 class CloudflareSpeedCli < Formula
   desc "CLI for internet speed test via cloudflare"
   homepage "https://github.com/kavehtehrani/cloudflare-speed-cli"
-  version "0.5.0"
+  version "0.6.5"
 
   on_macos do
     on_arm do
       url "https://github.com/kavehtehrani/cloudflare-speed-cli/releases/download/v#{version}/cloudflare-speed-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "e9795e7715820c817511f5e5b4fe6ca3448281a298d065053a3073212f9d9bfe"
+      sha256 "684314fab0098931cb13ce487a7e9e77436a96ad7a7d7c2218818dd85b682eae"
     end
     on_intel do
       url "https://github.com/kavehtehrani/cloudflare-speed-cli/releases/download/v#{version}/cloudflare-speed-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "3d8cba59df6a057c02ea4b9b5d7ad23515159120e53c6fe0923aedfd493112db"
+      sha256 "176cd0c80ff7ab6b92d61f099f5c0d02894e6e3bb9ecfd07dec3f6b060c61822"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/kavehtehrani/cloudflare-speed-cli/releases/download/v#{version}/cloudflare-speed-cli-aarch64-unknown-linux-musl.tar.xz"
+      sha256 "020bceb2c96c5cb01b03dd997c13d08577d4ff85054d54fb040ab7efd74dd37c"
+    end
+    on_intel do
+      url "https://github.com/kavehtehrani/cloudflare-speed-cli/releases/download/v#{version}/cloudflare-speed-cli-x86_64-unknown-linux-musl.tar.xz"
+      sha256 "b2d76823eaee7fa78db952ea2d233b29686d55f3df806629db213e6eeb2d4e48"
     end
   end
 
